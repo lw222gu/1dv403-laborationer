cd93ae67465c061864c3312246dbd10920088474
Lisa Westlund <lw222gu@student.lnu.se>
2014-11-09 19:35:57 +0000

    Slumpat secret och gjort en if-sats som kontrollerar det gissade numret

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,7 +2,7 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor( Math.random() * 100)+1; // Detta tal behöver bytas ut mot ett slumpat tal.
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
@@ -10,7 +10,24 @@ window.onload = function(){
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 			
 		// Plats för förändring.
-
+		var numberOfGuesses;
+		
+		if (number === secret){
+			return [true, "Grattis du vann! Det hemliga talet var " secret "och du behövde " numberOfGuesses "gissningar för att hitta det."];
+		}
+		
+		else if (0 < number < secret){
+			return [false, "Det hemliga talet är högre!"];
+		}
+		
+		else if (secret < number <= 100){
+			return [false, "Det hemliga talet är lägre!"];
+		}
+		
+		else {
+			return [false, "Talet du gissat på är utanför det slutna intervallet 1-100."];
+		};
+		
 
 		// Returnera exempelvis: 
 		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
