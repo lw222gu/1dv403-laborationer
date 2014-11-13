35c4f622ddc95347b5cc9482fd22b212b3b671fe
Lisa Westlund <lw222gu@student.lnu.se>
2014-11-13 14:58:30 +0000

    gjort grunden för funktionerna i message.js, samt fixat getters och setters

--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -1,5 +1,35 @@
 /* Message constructor */
 
-function Message(){
+function Message(message, date){
     
+    var text;
+    
+    this.getText = function(){
+        return message;
+    };
+    
+    this.setText = function(_text){
+        message = text;
+    };
+    
+    this.getDate = function(){
+        return date;
+    };
+    
+    this.setDate = function(_date){
+        date = new Date();
+    };
 }
+
+Message.prototype.toString = function(){
+    return this.getText()+" ("+this.getDate()+")";
+};
+
+Message.prototype.getHTMLText = function(_text){
+    
+};
+
+Message.prototype.getDateText = function(_date){
+    
+};
+
