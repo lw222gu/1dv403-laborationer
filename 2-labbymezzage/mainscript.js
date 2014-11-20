a3ec7cacb1fc62fe2ec9891ff45472818d0d6a11
Lisa Westlund <lw222gu@student.lnu.se>
2014-11-20 20:54:30 +0000

    försöker skapa objekt

--- 2-labbymezzage/mainscript.js
+++ 2-labbymezzage/mainscript.js
@@ -5,13 +5,37 @@ var mezzageApp = {
     messages: [],
 
     init: function(e){ 
+        var button = document.querySelector(".button");
+        button.onclick = function(){
         
-//        var button = document.getElementsByClass("button");
+        var mess = new Message(Message.prototype.getHTMLText(), Message.prototype.getDateText());
+        mezzageApp.messages.push(mess);
+        
+        console.log(mezzageApp.messages);
+
+        
+        for(var i = 0; i <= mezzageApp.messages.length - 1; i++){
+            
+            if (mezzageApp.messages.length == 1){
+                var ul = document.createElement("ul");
+                document.getElementById("messages").appendChild(ul);
+            }
+            
+            var li = document.createElement("li");
+//            li.innerHTML = Message.prototype.toString();
+            ul.appendChild(li);
+        }
+        
+ //       return false;
+        };
+        
+        
+        
 //        sendMess.addEventListener("click", function(){
 //           var mess = new Message();
 //        });
         
-     
+   /* 
         var mess = new Message("Testmeddelande", new Date());
         alert(mess);
         alert(mess.getText());
@@ -20,7 +44,8 @@ var mezzageApp = {
         
         mezzageApp.messages.push(mess, "Kiwi");
         alert(mezzageApp.messages[1]);
-
+        console.log(mess);
+    */
     }
 };
 
