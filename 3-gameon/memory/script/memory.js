02690b5d3f678be941ec44521f79f9f0f682444d
Lisa Westlund <lw222gu@student.lnu.se>
2014-12-03 11:45:06 +0000

    funktion som skapar arrayen med slumpade siffror

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,9 +1,19 @@
 "use strict";
 
 var Memory = {
+
+    imageArray: [],
+
     
     init: function(){
+        var rows = 4;
+        var cols = 4;
         
+        Memory.createGameArray(rows, cols);
+    },
+    
+    createGameArray: function (rows, cols){
+        var gameArray = RandomGenerator.getPictureArray(rows, cols);
     },
     
 };
