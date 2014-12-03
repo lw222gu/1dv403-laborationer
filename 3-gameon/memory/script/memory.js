0ab512a278c83463c1f3a9e0ab8fdf8ea2aa92b0
Lisa Westlund <lw222gu@student.lnu.se>
2014-12-03 12:19:30 +0000

    generar tabellen utifrån antalet rader och kolumner

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -4,16 +4,34 @@ var Memory = {
 
     imageArray: [],
 
-    
     init: function(){
-        var rows = 4;
-        var cols = 4;
+        var rows = 3;
+        var cols = 2;
         
         Memory.createGameArray(rows, cols);
     },
     
     createGameArray: function (rows, cols){
-        var gameArray = RandomGenerator.getPictureArray(rows, cols);
+        this.imageArray = RandomGenerator.getPictureArray(rows, cols);
+        console.log(this.imageArray);
+        Memory.createBoard(rows, cols);
+    },
+    
+    createBoard: function(rows, cols){
+//        var numberOfTiles = rows * cols;
+        var table = document.querySelector(".memorytable");
+        
+        for (var i = 1; i <= rows; i++){
+            var tableRow = document.createElement("tr");
+            tableRow.className = "tablerow";
+            table.appendChild(tableRow);
+            
+            for (var j = 1; j <= cols; j++ ){
+                var imageTile = document.createElement("td");
+                imageTile.className = "imageTile";
+                tableRow.appendChild(imageTile);
+            }
+        }
     },
     
 };
