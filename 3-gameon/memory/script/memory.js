530ede1d76ae980b233b46ce37d48d9f1ed84cef
Lisa Westlund <lw222gu@student.lnu.se>
2014-12-06 14:56:45 +0000

    onclickfunktion som vänder den bild som man klickar på

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -14,43 +14,56 @@ var Memory = {
     createGameArray: function (rows, cols){
         this.imageArray = RandomGenerator.getPictureArray(rows, cols);
         console.log(this.imageArray);
+    
         Memory.createBoard(rows, cols);
+        
+            
     },
     
     createBoard: function(rows, cols){
-//        var numberOfTiles = rows * cols;
         var table = document.querySelector(".memorytable");
-        
-        for (var i = 1; i <= rows; i++){
-            var tableRow = document.createElement("tr");
-            tableRow.className = "tablerow";
-            table.appendChild(tableRow);
-            
-            for (var j = 1; j <= cols; j++ ){
-                var tileCell = document.createElement("td");
-                tileCell.className = "imageTile";
-                tableRow.appendChild(tileCell);
-                
-                var aTile = document.createElement("a");
-                aTile.className = "tilelink";
-                aTile.setAttribute("href", "#");
+        var brickNumber = 0;
+
+
+            for (var i = 1; i <= rows; i++){
+                var tableRow = document.createElement("tr");
+                tableRow.className = "tablerow";
+                table.appendChild(tableRow);
+    
+                for (var j = 1; j <= cols; j++ ){
+                    var tileCell = document.createElement("td");
+                    tileCell.className = "imageTile";
                     
-                var tile = document.createElement("img");
-                tile.className = "tile";
-                tile.setAttribute("src", "pics/0.png")
- 
-//                this.imageArray.forEach(function(value){
-//                     tile.setAttribute("src", "pics/" + value + ".png");
-//                });
+                    var aTile = document.createElement("a");
+                    aTile.className = "tilelink";
+                    aTile.setAttribute("href", "#");
+                    aTile.setAttribute("bricknumber", brickNumber);
+                        
+                    var tile = document.createElement("img");
+                    tile.className = "tile";
+                    tile.setAttribute("src", "pics/0.png");
+                        
+                    aTile.appendChild(tile);
+                    tileCell.appendChild(aTile);
+                    tableRow.appendChild(tileCell);
+                    brickNumber += 1;
+                 
+                    aTile.onclick = function(e){
+                        var imageElement = this.getElementsByTagName("img");
+                        Memory.flipTile(imageElement[0], this.getAttribute("bricknumber"));
+                    }
                     
-                aTile.appendChild(tile);
-                tileCell.appendChild(aTile);
+                };
                     
             }
-        }
-        
         
     },
+    
+    flipTile: function(imageElement, bricknumber){
+            
+        imageElement.setAttribute("src", "pics/" + Memory.imageArray[bricknumber] + ".png");
+        
+    },    
     
 };
 
