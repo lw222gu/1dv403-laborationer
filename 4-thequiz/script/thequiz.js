69bef7c93d031d5cfb18acdc86667c2415b51fcb
Lisa Westlund <lw222gu@student.lnu.se>
2014-12-11 20:17:23 +0000

    hämtar fråga ett

--- 4-thequiz/script/thequiz.js
+++ 4-thequiz/script/thequiz.js
@@ -2,10 +2,34 @@
 
 var Quiz = {
     
-    questionNumber: 0,
-    
+    questionNumber: 1,
+
     init: function(){
         
+        var xhr = new XMLHttpRequest();
+        var question;
+        
+            xhr.onreadystatechange = function(){
+                
+                if (xhr.readyState === 4 && xhr.status === 200){
+                    question = JSON.parse(xhr.responseText);
+                    Quiz.generateQuestion(question.question);
+                }
+                
+            };
+            
+            xhr.open("GET", "http://vhost3.lnu.se:20080/question/" + Quiz.questionNumber, true);
+            xhr.send(null);
+
+            
+//        document.getElementById("button").onclick = function();
+        
+    },
+    
+    generateQuestion: function(question){
+        
+        console.log(question);
+    
     },
     
 };
