8cf78488f77374f4ab222410b075c6a6836d0335
Lisa Westlund <lw222gu@student.lnu.se>
2014-12-13 15:03:19 +0000

    loggar svaret när det skickas med submit eller enter

--- 4-thequiz/script/thequiz.js
+++ 4-thequiz/script/thequiz.js
@@ -3,40 +3,61 @@
 var Quiz = {
     
     questionNumber: 1,
+    answer: {},
+    questionText: "",
+    questionID: 1,
 
     init: function(){
         
         var xhr = new XMLHttpRequest();
-        var question;
         
             xhr.onreadystatechange = function(){
                 
                 if (xhr.readyState === 4 && xhr.status === 200){
-                    question = JSON.parse(xhr.responseText);
-                    Quiz.generateQuestion(question.question);
+                    var question = JSON.parse(xhr.responseText);
+                    Quiz.questionText = question.question;
+                    Quiz.questionID = question.id;
+                    Quiz.generateQuestion(Quiz.questionText, Quiz.questionID);
                 }
                 
             };
             
-            xhr.open("GET", "http://vhost3.lnu.se:20080/question/" + Quiz.questionNumber, true);
+            xhr.open("GET", "http://vhost3.lnu.se:20080/question/" + Quiz.questionID, true);
             xhr.send(null);
-
-            
-//        document.getElementById("button").onclick = function();
         
     },
     
-    generateQuestion: function(question){
+    generateQuestion: function(questionText, questionID){
         
-        console.log(question);
+        console.log(questionText);
         var questionHeader = document.getElementById("questionheader");
-        var questionText = document.getElementById("question");
+        var questionP = document.getElementById("question");
         
-        questionHeader.innerHTML = "Fråga " + Quiz.questionNumber;
-        questionText.innerHTML = question;
+        questionHeader.innerHTML = "Fråga " + questionID;
+        questionP.innerHTML = questionText;
         
         Quiz.questionNumber++;
+        
+        Quiz.saveAnswer();
     
+    },
+    
+    saveAnswer: function(){
+        
+        var textarea = document.getElementById("textbox");
+        var button = document.getElementById("button");
+
+        button.onclick = function(){
+            console.log(textarea.value);
+        };
+        
+        textarea.onkeypress = function(e){
+            if(e.keyCode == 13 && !e.shiftKey){
+                e.preventDefault();
+                console.log(textarea.value);
+            }
+        };
+        
     },
     
 };
