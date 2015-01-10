52e20930c10b7155b964fd603f0eef9214c9157b
Lisa Westlund <lw222gu@student.lnu.se>
2015-01-10 15:58:18 +0000

    första filstruktur för projektet

---
+++ 5-pwd/script/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var MyDesktop = {
+    
+    init: function(){
+        alert("test");
+    },
+};
+
+window.onload = MyDesktop.init; //pekar på metoden init som kommer köras när sidan laddats
